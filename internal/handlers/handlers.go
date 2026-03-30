2888bbcd05aa51d382cfd29374074b70f8359281
Karina Ikhsanova <karina23twins@yandex.ru>
2026-03-30 20:35:50 +0300

    first commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,90 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func HandleReturnHtml(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		log.Println("error: The GET method is expected")
+		http.Error(w, "error: The GET method is expected", http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := os.Stat("/home/arina/go1fl-sprint6-final-boss/index.html"); os.IsNotExist(err) {
+		log.Println("error: file is not exist")
+		http.Error(w, "error: file is not exist", http.StatusInternalServerError)
+		return
+	} else if err != nil {
+		log.Println("error checking file: ", err)
+		http.Error(w, "error checking file", http.StatusInternalServerError)
+		return
+	}
+
+	http.ServeFile(w, r, "/home/arina/go1fl-sprint6-final-boss/index.html")
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20) // 10 MB
+	if err != nil {
+		log.Println("error parsing form: ", err)
+		http.Error(w, "error parsing form", http.StatusInternalServerError)
+		return
+	}
+
+	// получаем файл из формы
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		log.Println("error receiving the file", err)
+		http.Error(w, "error receiving the file", http.StatusInternalServerError)
+		return
+	}
+	// закрываем файл
+	defer file.Close()
+
+	// читаем данные из файла
+	data, err := io.ReadAll(file)
+	if err != nil {
+		log.Println("error reading file:", err)
+		http.Error(w, "error reading file", http.StatusInternalServerError)
+		return
+	}
+
+	// передаем эти данные в функцию автоопределения из пакета service
+	convertedString, err := service.ConvertString(string(data))
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	// создаём локальный файл
+	// localFileName := time.Now().UTC().String() + filepath.Ext(handler.Filename)
+	localFileName := time.Now().UTC().Format("2006-01-02_15-04-05") + filepath.Ext(handler.Filename)
+	localFile, err := os.Create(localFileName)
+	if err != nil {
+		log.Println("error creating file:", err)
+		http.Error(w, "error creating file", http.StatusInternalServerError)
+		return
+	}
+	defer localFile.Close()
+
+	// записываем в локальный файл конвертированную строку
+	_, err = localFile.Write([]byte(convertedString))
+	if err != nil {
+		log.Println("error writing to file:", err)
+		http.Error(w, "error writing to file", http.StatusInternalServerError)
+		return
+	}
+
+	//	возврат результата конвертации строки:
+	fmt.Fprintln(w, convertedString)
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"errors"
+	"log"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func ConvertString(s string) (string, error) {
+	if s == "" || len(strings.TrimSpace(s)) == 0 {
+		log.Println("error: an empty string was passed")
+		return "", errors.New("error: an empty string was passed")
+	}
+
+	for _, r := range s {
+		if r != '.' && r != '-' && r != ' ' {
+			morseString := morse.ToMorse(s)
+			return morseString, nil
+		}
+	}
+
+	textString := morse.ToText(s)
+	return textString, nil
+}
